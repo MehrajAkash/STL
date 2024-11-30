8cf5800892479eef9ccde323769bdb0b98300296
Md. Mehraj Hossain Akash <mehrajakash81@gmail.com>
2024-11-30 21:18:00 +0600

    Queue Basic and some problem

---
+++ Queue/Queue Practice/Queue Reversal.cpp
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//function Template for C++
+
+//Function to reverse the queue.
+class Solution
+{
+    public:
+    queue<int> rev(queue<int> q)
+    {
+        // add code here.
+        queue<int> retq;
+        stack<int> ns;
+
+        while( !q.empty() ){
+            ns.push( q.front() );
+            q.pop();
+        }
+
+        while( !ns.empty() ){
+            retq.push( ns.top() );
+            ns.pop();
+        }
+
+        return retq;
+
+    }
+};
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int test;
+    cin>>test;
+    while(test--)
+    {
+    queue<int> q;
+    int n, var;
+    cin>>n;
+    while(n--)
+    {
+        cin>>var;
+        q.push(var);
+    }
+    Solution ob;
+    queue<int> a=ob.rev(q);
+    while(!a.empty())
+    {
+        cout<<a.front()<<" ";
+        a.pop();
+    }
+    cout<<endl;
+
+cout << "~" << "\n";
+}
+}
+// } Driver Code Ends
