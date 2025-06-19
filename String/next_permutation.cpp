93886107966cdeeb1f3254886aee97425bf36d5d
Md. Mehraj Hossain Akash <mehrajakash81@gmail.com>
2025-06-19 16:47:35 +0600

    Loading..........

--- String/next_permutation.cpp
+++ String/next_permutation.cpp
@@ -3,14 +3,37 @@ using namespace std;
 int main() {
 
     int t;
+
 	string s;
+
 	cin>>t;
-	while(t--){
+	for(int l=1;l<=t; l++){
 
-        cin>>s;
+        int k,n; cin>>k>>n;
 
-        if( next_permutation( s.begin(), s.end()) ) cout<< s <<endl;
-        else cout<< "no answer" <<endl;
+        string s;
+
+        int val=65;
+        for(int i=0; i<k;i++){
+            s.append(1,char(val)); /** storing Letter from A to A+k in string **/
+            val++;
+        }
+
+        cout<<"Case "<<l<<":"<<endl;
+        cout<<s<<endl;
+
+        for(int i=2;i<=n; i++){
+             if( next_permutation( s.begin(), s.end()) ) cout<< s <<endl;
+
+             else{
+                break;
+             }
+        }
+
 	}
 
 }
+
+/**
+ ProblemLink: https://lightoj.com/problem/discovering-permutations#google_vignette
+**/
